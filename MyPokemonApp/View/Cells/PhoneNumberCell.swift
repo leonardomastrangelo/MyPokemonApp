e003ee649c09777b72d16291651f311f0bdcff49
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2024-10-22 10:03:32 +0200

    PhoneNumber added

---
+++ MyPokemonApp/View/Cells/PhoneNumberCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhoneNumberCell.swift
+//  MyPokemonApp
+//
+//  Created by Leonardo Mastrangelo on 22/10/24.
+//
+
+import UIKit
+
+class PhoneNumberCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
